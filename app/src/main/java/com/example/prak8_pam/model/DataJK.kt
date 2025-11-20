16bf8fcb3f3e5dbaf2adf9e0652adf4f8139660c
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-20 14:04:18 +0700

    Rename typo

--- app/src/main/java/com/example/prak8_pam/model/DataJK.kt
+++ app/src/main/java/com/example/prak8_pam/model/DataJK.kt
@@ -3,7 +3,7 @@ package com.example.prak8_pam.model
 import com.example.prak8_pam.R
 
 object DataJK {
-    val jenisK = listOf(
+    val JenisK = listOf(
         R.string.laki,
         R.string.wanita
     )
