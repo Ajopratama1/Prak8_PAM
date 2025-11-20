d0da21e896f6ab2b75867bd85ead9953b6625fd9
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-20 12:31:29 +0700

    Implementasi navigasi aplikasi menggunakan NavHost
    Tambahkan Composable `DataApp` yang berfungsi sebagai NavHost utama aplikasi.
    - Mendefinisikan rute enum `Navigasi` (Formulirku, Detail).
    - Mengintegrasikan Composable `FormSiswa` dan `TampilSiswa`.
    - Mengatur aksi navigasi:
        - Tombol 'Submit' di `FormSiswa` menavigasi ke layar Detail.
        - Tombol 'Kembali' di `TampilSiswa` menggunakan `popBackStack` untuk kembali ke Formulirku.

---
+++ app/src/main/java/com/example/prak8_pam/view/uicontroller/Navigasi.kt
@@ -0,0 +1,60 @@
+package com.example.prak8_pam.view.uicontroller
+
+
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.prak8_pam.view.FormSiswa
+import com.example.prak8_pam.view.TampilSiswa
+
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormSiswa(
+                    OnSubmitBtnClick = {
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+
+                )
+            }
+            composable(route = Navigasi.Detail.name) {
+                TampilSiswa(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulirku(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulirku(
+    navController: NavHostController
+) {
+    navController.popBackStack(Navigasi.Formulirku.name,
+        inclusive = false)
+}
+
