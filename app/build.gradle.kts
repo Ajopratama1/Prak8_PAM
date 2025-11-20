3c7d38660e59c56129b938bfa8ee5fd6df02f0ba
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-20 14:25:20 +0700

    menghapus matrial3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
-    implementation(libs.androidx.material3)
+
 
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
