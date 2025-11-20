fb6d9b6ee4e38f735fcaf697a2d3d4df07e8d58a
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-20 12:22:49 +0700

    mengubah function pada MainActivity.kt

--- app/src/main/java/com/example/prak8_pam/MainActivity.kt
+++ app/src/main/java/com/example/prak8_pam/MainActivity.kt
@@ -7,11 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.prak8_pam.ui.theme.Prak8_PAMTheme
+import com.example.prak8_pam.ui.theme.Prak8_PAMTheme
+import com.example.prak8_pam.view.uicontroller.DataApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +19,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             Prak8_PAMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataApp(
+
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Prak8_PAMTheme {
-        Greeting("Android")
     }
 }
