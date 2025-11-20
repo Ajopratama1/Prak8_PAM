95a67ef7ba2a657d6a5a68f47dd40d5113ad5f52
Ajopratama1 <a.ajopratama01@gmail.com>
2025-11-20 13:30:01 +0700

    Implementasi SiswaViewModel untuk manajemen State UI

---
+++ app/src/main/java/com/example/prak8_pam/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.prak8_pam.viewmodel
+
+import com.example.prak8_pam.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel {
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls:MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
+
+}
